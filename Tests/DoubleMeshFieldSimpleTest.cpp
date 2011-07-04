a62f1f3db3656743c1071e6bac57d8105a849e24
philippe.jacquet.0@gmail.com <philippe.jacquet.0@gmail.com@7081400c-9f7b-7fb5-78e1-0d209a21d8d6>
2011-07-04 22:05:55 +0000

    Test IO DMF

--- Tests/DoubleMeshFieldSimpleTest.cpp
+++ Tests/DoubleMeshFieldSimpleTest.cpp
@@ -78,13 +78,49 @@ void constructorTest() {
     }
 }
 
+void ioTest() {
+    double gb[] = {18.0E6, 1E0, 1.0E-11};
+    double nodes[] = {0., 5., 10., 20.}; 
+    Mesh * mesh1 = new EnergyMesh(gb, 3);
+    Mesh * mesh2 = new CartesianOneDimMesh(nodes, 4);
+
+    vector< pair< Mesh*, DoubleMeshField::OptionMeshField> > meshes ;
+    meshes.push_back(pair< Mesh*, DoubleMeshField::OptionMeshField>(mesh1, DoubleMeshField::FULL)) ;
+    meshes.push_back(pair< Mesh*, DoubleMeshField::OptionMeshField>(mesh2, DoubleMeshField::LAZY)) ;
+    DoubleMeshField dmf(meshes);
+    vector<string> reg ;
+
+    reg.clear() ;
+    reg.push_back("0") ;
+    reg.push_back("1") ;
+    dmf.buildFamily(1, reg, "A") ;
+    try {
+        cout << "subtest 1" << endl;
+        dmf.buildData() ;
+        cout << "passed" << endl;
+        cout << "subtest 2" << endl;
+        cout<<dmf.getDouble(&FieldIterator::create("0","0"))<<endl;
+        dmf.setDouble(&FieldIterator::create("0","0"),1.) ;
+        cout<<dmf.getDouble(&FieldIterator::create("0","0"))<<endl;
+        cout << "passed" << endl;
+    }    catch (const exception & e) {
+        cout << e.what() << endl;
+        cout << "%TEST_FAILED% time=0 testname=ioTest (DoubleMeshFieldTest) message=Unexpected Exception" << endl;
+    } 
+}
+
+
+
+
 int main(int argc, char** argv) {
     std::cout << "%SUITE_STARTING% DoubleMeshFieldTest" << std::endl;
     std::cout << "%SUITE_STARTED%" << std::endl;
 
     std::cout << "%TEST_STARTED% constructorTest (DoubleMeshFieldTest)" << std::endl;
     constructorTest();
-    std::cout << "%TEST_FINISHED% time=0 constructorTest (DoubleMeshFieldTest)" << std::endl;
+    std::cout << "%TEST_STARTED% constructorTest (DoubleMeshFieldTest)" << std::endl;
+    ioTest();
+    std::cout << "%TEST_FINISHED% time=0 ioTest (DoubleMeshFieldTest)" << std::endl;
 
     
     std::cout << "%SUITE_FINISHED% time=0" << std::endl;
