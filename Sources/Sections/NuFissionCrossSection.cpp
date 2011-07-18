8755e42cea0ebcfc9a782c2a0e5554fe596cc6c7
philippe.jacquet.0@gmail.com <philippe.jacquet.0@gmail.com@7081400c-9f7b-7fb5-78e1-0d209a21d8d6>
2011-07-18 17:07:21 +0000

    Rename: FissionCrossSection -> NuFissionCrossSection

---
+++ Sources/Sections/NuFissionCrossSection.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   NuFissionCrossSection.cpp
+ * Author: Philippe Jacquet <contact@philippe-jacquet.com>
+ * 
+ * Created on 1 juin 2011, 22:54
+ */
+
+#include "NuFissionCrossSection.h"
+
+NuFissionCrossSection::NuFissionCrossSection() {
+}
+
+NuFissionCrossSection::NuFissionCrossSection(const NuFissionCrossSection& orig) {
+}
+
+NuFissionCrossSection::~NuFissionCrossSection() {
+}
+
