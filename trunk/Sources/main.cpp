a62c62ec3be6ab4e2bb9b3a22f6b364b21f53dba
philippe.jacquet.0@gmail.com <philippe.jacquet.0@gmail.com@7081400c-9f7b-7fb5-78e1-0d209a21d8d6>
2011-12-12 23:03:56 +0000

    Construction d'une library light de test

--- trunk/Sources/main.cpp
+++ trunk/Sources/main.cpp
@@ -52,31 +52,30 @@ int main(int argc, char** argv) {
         cout << spatialMesh->toString() << endl;
         cout << lib->getEnergyMesh()->toString()<<endl;
 
+        string combustible1Regions[] = {"0","1","2","3","4"} ;
+        string combustible2Regions[] = {"5"} ;
+        string combustible3Regions[] = {"6","7","8","9"} ;
+
         vector<string> regionsName;
         vector< pair< string, double > > nucleiConcentration ;
 
-        regionsName.clear(); nucleiConcentration.clear() ;
-        regionsName.push_back("0");
-        regionsName.push_back("1");
-        regionsName.push_back("2");
-        regionsName.push_back("3");
-        regionsName.push_back("4");
-        nucleiConcentration.push_back( pair<string,double>("U235", 0.1) ) ;
-        nucleiConcentration.push_back( pair<string,double>("U238", 1.0) ) ;
-        nucleiConcentration.push_back( pair<string,double>("016",  2.2) ) ;
+        regionsName.resize(sizeof(combustible1Regions)/sizeof(string));
+        copy ( combustible1Regions, combustible1Regions+sizeof(combustible1Regions)/sizeof(string), regionsName.begin() );
+        nucleiConcentration.clear() ;
+        nucleiConcentration.push_back( pair<string,double>("U235", 0.5) ) ;
+        nucleiConcentration.push_back( pair<string,double>("O16",  1.0) ) ;
         geometry.fill("combustible",regionsName,nucleiConcentration);
 
-        regionsName.clear(); nucleiConcentration.clear() ;
-        regionsName.push_back("5");
-        nucleiConcentration.push_back( pair<string,double>("Fe56", 1.0) ) ;
+        regionsName.resize(sizeof(combustible2Regions)/sizeof(string));
+        copy ( combustible2Regions, combustible2Regions+sizeof(combustible2Regions)/sizeof(string), regionsName.begin() );
+        nucleiConcentration.clear() ;
+        nucleiConcentration.push_back( pair<string,double>("U235", 1.0) ) ;
         geometry.fill("gaine",regionsName,nucleiConcentration);
 
-        regionsName.clear(); nucleiConcentration.clear() ;
-        regionsName.push_back("6");
-        regionsName.push_back("7");
-        regionsName.push_back("8");
-        regionsName.push_back("9");
-        nucleiConcentration.push_back( pair<string,double>("H20", 1.0) ) ;
+        regionsName.resize(sizeof(combustible3Regions)/sizeof(string));
+        copy ( combustible3Regions, combustible3Regions+sizeof(combustible3Regions)/sizeof(string), regionsName.begin() );
+        nucleiConcentration.clear() ;
+        nucleiConcentration.push_back( pair<string,double>("O16", 1.0) ) ;
         geometry.fill("modérateur",regionsName,nucleiConcentration);
 
 
